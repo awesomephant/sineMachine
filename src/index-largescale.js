fcdbff8a11df42884c9463bd7c13af60f6428cde
Max Koehler <max@koehler-kn.de>
2018-06-14 13:04:59 +0100

    Start to build out pen constraining

--- src/index-largescale.js
+++ src/index-largescale.js
@@ -67,8 +67,62 @@ var config = {
     aOffsetAxis: 'y',
     bOffsetAxis: 'y',
     drawingScale: 2,
-	useDerivative: false
+    useDerivative: false,
+    artBoard: {
+        width: 200,  //mm
+        height: 200,
+        x: 10,
+        y: 10
+    },
+    wheelDiameter: 3, //Stepper wheel radius in mm
+    origin: {
+        x: 0,
+        y: 0
+    },
 }
+
+const intersectCircles = function (x1, y1, r1, x2, y2, r2) {
+    var centerdx = x1 - x2;
+    var centerdy = y1 - y2;
+    var R = Math.sqrt(centerdx * centerdx + centerdy * centerdy);
+    if (!(Math.abs(r1 - r2) <= R && R <= r1 + r2)) { // no intersection
+        return []; // empty list of results
+    }
+    // intersection(s) should exist
+
+    var R2 = R * R;
+    var R4 = R2 * R2;
+    var a = (r1 * r1 - r2 * r2) / (2 * R2);
+    var r2r2 = (r1 * r1 - r2 * r2);
+    var c = Math.sqrt(2 * (r1 * r1 + r2 * r2) / R2 - (r2r2 * r2r2) / R4 - 1);
+
+    var fx = (x1 + x2) / 2 + a * (x2 - x1);
+    var gx = c * (y2 - y1) / 2;
+    var ix1 = fx + gx;
+    var ix2 = fx - gx;
+
+    var fy = (y1 + y2) / 2 + a * (y2 - y1);
+    var gy = c * (x1 - x2) / 2;
+    var iy1 = fy + gy;
+    var iy2 = fy - gy;
+
+    // note if gy == 0 and gx == 0 then the circles are tangent and there is only one solution
+    // but that one solution will just be duplicated as the code is currently written
+    return [[ix1, iy1], [ix2, iy2]];
+}
+
+/**
+ * Converts a number of steps to mm, given the diameter of the output wheels and the microstepping resolution
+ * @param {Float} Steps
+ * @returns {number} Length in mm
+ */
+const stepsToLength = function (steps) {
+    let c = 2 * Math.PI * Math.pow(config.wheelDiameter, 2);
+    let length = steps * (c / (config.stepperResolution * config.microstepResolution))
+    return length;
+}
+
+
 var moveStepperTo = function (stepper, n, cb) {
     stepper.isMoving = true;
     let delta = 0;
@@ -84,8 +138,18 @@ var moveStepperTo = function (stepper, n, cb) {
     if (speed >= config.maxRPM){
         speed = config.maxRPM
     }
+    let inters = intersectCircles(config.stepperAPos.x, config.stepperAPos.y, stepsToLength(stepperA.currentPosition), config.stepperBPos.x, config.stepperBPos.y, stepsToLength(stepperB.currentPosition));
+    
+    let currentCoordinates = [inters[1]]
 
-    //    console.log('Moving by ' + delta + ' steps');
+    if (currentCoordinates[0] < config.artBoard.x ||
+        currentCoordinates[0] > config.artBoard.x + config.artBoard.width ||
+        currentCoordinates[1] < config.artBoard.y ||
+        currentCoordinates[1] > config.artBoard.y + config.artBoard.height
+    ) {
+        console.log('Outside the limits')
+    }
+
     stepper.rpm(speed).direction(direction).step(Math.abs(delta * 5), function () {
         stepper.currentPosition += delta;
         stepper.isMoving = false;
@@ -108,8 +172,8 @@ var setStepperSpeed = function (stepper, s, cb) {
 
 //    console.log('Setting speed to ' + speed);
 
-    stepper.rpm(Math.abs(speed)).direction(direction).step(5, function () { //  20 steps is arbitrary
-        stepper.currentPosition += 20 * direction;
+    stepper.rpm(Math.abs(speed)).direction(direction).step(5, function () { //  5 steps is arbitrary
+        stepper.currentPosition += 5 * direction;
         stepper.isMoving = false;
         io.emit('stepper ' + stepper.max_name + ' status', stepper.currentPosition)
         cb();
